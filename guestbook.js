def598a92e4c33f088e95613c528ff9e711cf260
Claudio Escudero Gamboa <107541289+cescuderog@users.noreply.github.com>
2022-07-22 08:57:28 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://b0cb90fd.eu-de.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
